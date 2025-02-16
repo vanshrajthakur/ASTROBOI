2f8fcf93f463e4b385e8def96dcc6a8e4dced2cd
vanshrajthakur <vanshsinghthakur3@gmail.com>
2025-02-16 23:08:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
   {
-      name: "INZEWORLD.COM (DE)",
-      password: "saher.inzeworld.com",
-      host: "lava.inzeworld.com"
-      port: 3128,
+      name: "Koi Node V4",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port:  3000,
       secure: false
     }
   ]
